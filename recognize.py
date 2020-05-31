5c9c349dc6e6ece622589c6a000d62a09c386c63
jimmytuc <jimmytucsolo@gmail.com>
2020-05-31 13:08:58 +0800

    init

---
+++ recognize.py
@@ -0,0 +1,22 @@
+# import the necessary packages
+import numpy as np
+import argparse
+import imutils
+import pickle
+import cv2
+import os
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True,
+	help="path to input image")
+ap.add_argument("-d", "--detector", required=True,
+	help="path to OpenCV's deep learning face detector")
+ap.add_argument("-m", "--embedding-model", required=True,
+	help="path to OpenCV's deep learning face embedding model")
+ap.add_argument("-r", "--recognizer", required=True,
+	help="path to model trained to recognize faces")
+ap.add_argument("-l", "--le", required=True,
+	help="path to label encoder")
+ap.add_argument("-c", "--confidence", type=float, default=0.5,
+	help="minimum probability to filter weak detections")
+args = vars(ap.parse_args())
